6c1c2ba2ae8e3bc4907a31d03bde016e0519669f
Denis Berezin <nocab139@yandex.ru>
2022-09-08 22:33:31 +0300

    #14 deploy

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ import "./styles/main.css";
 import Layout from "./pages/Layout/Layout";
 import Home from "./pages/Home/Home";
 import Order from "./pages/Order/Order";
+import {PUBLIC_URL} from "./utils/envConstants";
 
 const App = () => {
     return (
         <>
             <Routes>
-                <Route path="/" element={<Layout />}>
+                <Route path={`/`} element={<Layout />}>
                     <Route index element={<Home />} />
                     <Route path="/order" element={<Order />} />
                 </Route>
